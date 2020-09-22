f0f20658b6d69c7bba22a0133cc6354791a8ae6d
Waymouth <swaym@amazon.co.uk>
2020-09-22 09:02:24 +0100

    Updated the script to include the necessary logging stuff.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import flask, logging, watchtower
-from flask import request, jsonify
+from flask import request, jsonify, make_response
+from waitress import serve
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 logging.basicConfig(level=logging.INFO)
 handler = watchtower.CloudWatchLogHandler(log_group='cognito-learning', stream_name='api', create_log_group=False, use_queues=False)
 app.logger.addHandler(handler)
-logging.getLogger("werkzeug").addHandler(handler)
+app.logger.setLevel(logging.DEBUG)
+logging.getLogger("watiress").setLevel(logging.WARNING)
+logging.getLogger("watiress").addHandler(handler)
 
 secrets = [
     {
@@ -72,27 +75,59 @@ public = [
     
 ]
 
+homepage = '''
+    <html>
+        <h1>Hello world</h1>
+        <p>This site is a prototype API which does little.</p>
+        <p>Here is some public stuff...</p>
+        <a href="/v1/public">Public Stuff</a>
+        <p>Here is some secret stuff...</p>
+        <a href="/v1/secrets">Secret Stuff</a>
+        <p>Here is a logout link...</p>
+        <a href="https://sams-test-site.auth.us-east-1.amazoncognito.com/logout?client_id=6uri15vh9sig0e0j3fimc656m6&logout_uri=https://sams-test-site.com/logout">Logout</a>
+    </html>
+'''
+
+logoutpage = '''
+    <html>
+        <script>
+            function logout (){
+                window.location.href = "https://sams-test-site.com"
+            }
+        </script>
+        <body onload="logout();">
+    </html>
+'''
+
 @app.before_request
 def log_request_info():
-    app.logger.info('>>> Start Request.')
-    app.logger.info('URL: %s', request.url)
-    app.logger.info('Headers: %s', request.headers)
-    app.logger.info('Cookies: %s', request.cookies)
-    app.logger.info('Body: %s', request.get_data())
-    app.logger.info('<<< Finish Request.')
+    app.logger.debug('>>> Start Request.')
+    app.logger.debug('URL: %s', request.url)
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Cookies: %s', request.cookies)
+    app.logger.debug('Body: %s', request.get_data())
+    app.logger.debug('<<< Finish Request.')
 
 @app.after_request
 def after_request_func(response):
-    app.logger.info('>>> Start Response.')
-    app.logger.info('Headers: %s', response.headers)
-    app.logger.info('Body: %s', response.get_data())
-    app.logger.info('<<< Finish Response.')
+    app.logger.debug('>>> Start Response.')
+    app.logger.debug('Headers: %s', response.headers)
+    app.logger.debug('Body: %s', response.get_data())
+    app.logger.debug('<<< Finish Response.')
     return response
 
 # A default route
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Hello world</h1><p>This site is a prototype API which does little.</p>"
+    return homepage
+
+# A logout route
+@app.route('/logout', methods=['GET'])
+def logout():
+    res = make_response(logoutpage)
+    res.set_cookie('AWSELBAuthSessionCookie-0', max_age=0)
+    res.set_cookie('AWSELBAuthSessionCookie-1', max_age=0)
+    return res
 
 # A route to return all of the available entries in our catalog.
 @app.route('/v1/secrets', methods=['GET'])
@@ -104,4 +139,7 @@ def api_secrets():
 def api_public():
     return jsonify(public)
 
-app.run()
+if __name__ == "__main__":
+   #app.run() ##Replaced with below code to run it using waitress 
+   serve(app, host='0.0.0.0', port=5000)
+
