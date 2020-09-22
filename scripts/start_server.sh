481345be2053b73d699e35ba9c63ff52016794dd
Waymouth <swaym@amazon.co.uk>
2020-09-22 11:53:48 +0100

    Corrected log output for server.log.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export AWS_DEFAULT_REGION=us-east-1
 . /home/ec2-user/env/bin/activate
-python /home/ec2-user/app.py > server.log 2>&1 &
+python /home/ec2-user/app.py > /home/ec2-user/server.log 2>&1 &
